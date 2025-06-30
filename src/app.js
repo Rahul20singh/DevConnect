5c045b7150e80844006772c9c9f1f3b04a2f277d
Rahul Bisht <rahulbisht575@gmail.com>
2025-06-30 11:12:41 +0530

    created http: methods

--- src/app.js
+++ src/app.js
@@ -2,21 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.use("/homePage", (req, res) => {
-  res.send("Hello, from home page!");
-});
+app.use(express.json());
 
-app.use("/feedPage", (req, res) => {
-  res.send("Hello, from feed page!");
-});
 
-app.use("/loginPage", (req, res) => {
-  res.send("Hello, from login page!");
-});
-
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello, World!");
-});
+})
+
+app.get("/user", (req, res) => {
+  res.json({ name: "John Doe", age: 30 });  
+})
+
+
+app.post("/user", (req, res) => {
+  // const user = req.body;  
+  res.json({ name: "Ryan Doe", age: 30 })
+})
+
+
+app.delete("/user", (req, res) => {
+  // const user = req.body;  
+  res.json({ name: "Dan Doe", age: 30 })
+})
+
+app.put("/user", (req, res) => {
+  // const user = req.body; 
+  
+  res.json({ name: "Jane Doe", age: 30 })
+})
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
