9268a0e11d35207032a252a15bd228c410374ade
Rahul Bisht <rahulbisht575@gmail.com>
2025-08-14 02:45:03 +0530

    updated port

--- src/app.js
+++ src/app.js
@@ -32,8 +32,8 @@ app.use("/", userRouterouter);
 connectDB()
   .then(() => {
     console.log("Connected to MongoDB successfully");
-    app.listen(3000, () => {
-      console.log("Server is running on http://localhost:3000");
+    app.listen(3000, "0.0.0.0", () => {
+      console.log("Server is running on http://0.0.0.0:3000");
     });
   })
   .catch((err) => {
